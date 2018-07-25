ce8279cc3396bfa9315dfebe00b5fdf109277d86
mahaliahr <mahalia_hr@outlook.com>
2018-07-25 16:14:41 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,23 +137,12 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      # Should you want to write your own test for this situation,
-      # read the following code and comments.
-
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Xs?"
-
-      # Define the board with values that should create the desired output
-      # *** Edit the line below ***
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] # This is not correct
+      
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] 
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
-
-      # Each line that starts with expect represents a row in the ouput.
-      # The desired characters a row must include are provided by the String
-      # of the row. EX: The top row filled with X would be " X | X | X "
 
       expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
@@ -167,11 +156,8 @@ describe "#display_board in 'lib/display_board.rb" do
 
     it 'prints an entire board full of Os' do
 
-      # Define the board with values that should create the desired output
-      # *** Edit the line below ***
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] # This is not correct
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] 
 
-      # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
