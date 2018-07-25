64c2c0386e1aec8253473f0e793cfa3154ea646d
mahaliahr <mahalia_hr@outlook.com>
2018-07-25 16:05:38 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -183,10 +183,6 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
-      # Each line that starts with expect represents a row in the ouput.
-      # The desired characters a row must include are provided by the String
-      # of the row. EX: The top row filled with X would be " X | X | X "
-
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
